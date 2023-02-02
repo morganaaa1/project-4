48fb9dde79f987335351bdbeac80c91e2a751e2c
ali <alinabilahramadhan@gmail.com>
2023-02-02 12:01:16 +0700

    using a forward refs

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useRef, useImperativeHandle} from "react";
 
 import classes from './Input.module.css'
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
+
+    const inputRef = useRef();
+
+    const activate = () => {
+        inputRef.current.focus();
+    }
+
+    useImperativeHandle(ref, () => {
+        return {
+            focus: activate
+        }
+    });
+
     return (
     <div
         className={`${classes.control} ${
@@ -11,14 +24,15 @@ const Input = props => {
     >
         <label htmlFor={props.id}>{props.label}</label>
         <input
-          type={props.type}
-          id={props.id}
-          value={props.value}
-          onChange={props.onChange}
-          onBlur={props.onBlur}
+            ref={inputRef}
+            type={props.type}
+            id={props.id}
+            value={props.value}
+            onChange={props.onChange}
+            onBlur={props.onBlur}
         />
     </div>
     )
-}
+})
 
 export default Input
